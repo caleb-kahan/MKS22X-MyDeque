0314cc604d1569d90c7df34e2a9da4de89b9e80f
Caleb Kahan <ckahan00@stuy.edu>
2019-03-26 14:25:19 -0400

    variables initialized

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,9 @@ public class Calculator{
     MyDeque tool = new MyDeque(20);
     while(sys.hasNext()){
       if(! sys.hasNextInt()){
+        String operator = sys.next();
         int secondArg = Integer.parseInt(tool.removeLast());
+        int firstArg = Integer.parseInt(tool.removeLast());
       }
       else
         tool.add(sys);
