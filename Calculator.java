752817faa2098c4e5102702966c57b214378a0f4
Caleb Kahan <ckahan00@stuy.edu>
2019-03-26 14:39:54 -0400

    forgot it had to be anobject

--- Calculator.java
+++ Calculator.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 public class Calculator{
   public static double eval(String s){
     Scanner sys = new Scanner(s);
-    MyDeque<int> tool = new MyDeque(20);
+    MyDeque<Integer> tool = new MyDeque<Integer>(20);
     while(sys.hasNext()){
       if(! sys.hasNextInt()){
         char operator = sys.next().charAt(0);
-        int secondArg = tool.removeLast();
-        int firstArg = tool.removeLast();
+        double secondArg = tool.removeLast();
+        double firstArg = tool.removeLast();
         switch(operator){
           case '+':
-            return firstArg + 0.0 + secondArg;
+            return firstArg  + secondArg;
           case '-':
-            return firstArg - 0.0 - secondArg;
+            return firstArg - secondArg;
           case '*':
-            return firstArg * 1.0 * secondArg;
+            return firstArg * secondArg;
           case '/':
-            return firstArg * 1.0 / secondArg;
+            return firstArg / secondArg;
           case '%':
-            return firstArg * 1.0 * secondArg;
+            return firstArg % secondArg;
         }
       }
       else
