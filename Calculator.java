ae5b9c4ebfc135de95cf29ddfb6233e25fc9a248
Caleb Kahan <ckahan00@stuy.edu>
2019-03-27 14:53:39 -0400

    forgot to return the right thing

--- Calculator.java
+++ Calculator.java
@@ -3,12 +3,12 @@ public class Calculator{
   public static double eval(String s){
     Scanner sys = new Scanner(s);
     MyDeque<Double> tool = new MyDeque<Double>(20);
-    double result=0;
     while(sys.hasNext()){
-      if(! sys.hasNextInt() && ! sys.hasNextDouble()){
+      if(! sys.hasNextDouble()){
         char operator = sys.next().charAt(0);
         double secondArg = tool.removeLast();
         double firstArg = tool.removeLast();
+        System.out.println("run");
         switch(operator){
           case '+':
             tool.addLast(firstArg  + secondArg);
@@ -25,6 +25,6 @@ public class Calculator{
       else
         tool.addLast(sys.nextDouble());
     }
-    return result;
+    return tool.getFirst();
   }
 }
